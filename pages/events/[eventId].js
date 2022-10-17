fab904490918e653f7d51c0fabce5818eb4181f6
Alexey Piltenko <101993475+OleksiiPro1@users.noreply.github.com>
2022-10-17 17:32:25 +0200

    events on dinamic pages

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,34 @@
+import { useRouter } from 'next/router';
+import { Fragment } from 'react';
+import EventContent from '../../components/event-detail/event-content';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventSummary from '../../components/event-detail/event-summary';
+import { getEventById } from '../../dummy-data';
+
 export default function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return (
+      <h1 style={{ textAlign: 'center', margin: '200px' }}>No event found!</h1>
+    );
+  }
+
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <Fragment title={event.title}>
+      <EventSummary />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
