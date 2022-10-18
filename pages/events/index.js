05efdb5a863b63eeb26f8332723d78f93ffa1d36
Alexey Piltenko <101993475+OleksiiPro1@users.noreply.github.com>
2022-10-18 10:02:35 +0200

    Search events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,24 @@
+import router, { useRouter } from 'next/router';
+import { Fragment } from 'react';
+import EventsSearch from '../../components/events/event-search';
 import EventList from '../../components/events/EventList';
 import { getAllEvents } from '../../dummy-data';
 
 export default function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+
   return (
     <div>
-      <EventList items={events} />
+      <Fragment>
+        <EventsSearch onSearch={findEventHandler} />
+        <EventList items={events} />
+      </Fragment>
     </div>
   );
 }
